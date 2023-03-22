fa496c153e19de45319bf38dfef4d51fb19443fc
Trần An Tiến <89237543+supertien12@users.noreply.github.com>
2023-03-23 00:31:01 +0700

    add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500"
+    origin: "https://supertien12.github.io"
   }));
 
 app.use(express.json({limit: "50mb"}));
