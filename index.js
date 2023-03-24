38e19c73921f4695478510ff885b7554c2fc128d
Trần An Tiến <89237543+supertien12@users.noreply.github.com>
2023-03-24 23:54:05 +0700

    add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: "https://supertien12.github.io"
+    origin: "https://sinhhoatchinhtri-omega.vercel.app"
   }));
 
 app.use(express.json({limit: "50mb"}));
